d80627131eb5bac11b2a99a252f95dbcafc19719
anton <antonio.kolesnikow@gmail.com>
2026-05-02 11:38:30 +0300

    docs(separate-chaining): deleted odd comments

--- tests/ChainingTest.cpp
+++ tests/ChainingTest.cpp
@@ -26,7 +26,6 @@ int main()
     auto data = GenerateData();
     std::cout << "Data generating finish\n";
 
-    // ===== INIT BENCHMARK =====
     const auto umapInitStart = GetNow();
     std::unordered_map<DICT_CHAIN_KEY_TYPE, double> umap(data.begin(), data.end());
     const auto umapInitTime = DurationNs(umapInitStart, GetNow());
@@ -39,12 +38,10 @@ int main()
     }
     const auto dictInitTime = DurationNs(dictInitStart, GetNow());
 
-    // ===== ACCESS FUNCS =====
     const auto umapFunc = [&](const DICT_CHAIN_KEY_TYPE &key) { return umap[key]; };
 
     const auto dictFunc = [&](const DICT_CHAIN_KEY_TYPE &key) { return dict.GetValue(key); };
 
-    // ===== BENCHMARK READ =====
     const auto chainTime1 = Benchmark(data, dictFunc);
     const auto umapTime1 = Benchmark(data, umapFunc);
 
